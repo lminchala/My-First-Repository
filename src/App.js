b76082fb80d75887f2ffa3f982e422a70a0d973f
lminchala <73615264+lminchala@users.noreply.github.com>
2021-10-18 17:16:31 -0400

    added year and highschool

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1> Welcome my strange Familia</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2> 12th grade, Hudson Highschool of Learning Tenchologies</h2>
     </div>
   );
 }
