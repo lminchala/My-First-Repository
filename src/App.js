6622f71a210f08586af35d72d5c39237269f9411
lminchala <73615264+lminchala@users.noreply.github.com>
2021-10-25 17:54:09 -0400

    -_-

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Welcome my strange Familia</h1>
-      <h2> 12th grade, Hudson Highschool of Learning Tenchologies</h2>
+      <h1> Functions </h1>
     </div>
   );
 }
+
